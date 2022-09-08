ee198261cb44cb59100401d252a87a5d19224702
Ravi Chitroda <ravi.chitroda91@gmail.com>
2022-09-08 12:15:27 +0530

    Initialization

---
+++ src/App.js
@@ -0,0 +1,7 @@
+import "./App.css";
+
+function App() {
+  return <div className="App">Axios Tutorial</div>;
+}
+
+export default App;
---
+++ src/component/Title.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Title = () => {
+  return (
+    <div className="title">
+      <h1>
+        <span>axios</span> tutorial
+      </h1>
+    </div>
+  );
+};
+
+export default Title;
